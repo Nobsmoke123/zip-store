478e5ad0480dd0f6320a4b952f49c247e23c9d56
Nobsmoke123 <donaldakobundu@gmail.com>
2025-02-17 22:27:13 +0100

    Add react-router-dom and implement routing for HomePage and HatsPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
 import HomePage from "./pages/home/homepage.component";
+import { Route, Routes } from "react-router-dom";
+
+const HatsPage = () => (
+  <>
+    <h1>Welcome Hats Page</h1>
+  </>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path="/" Component={HomePage} />
+        <Route path="/hats" Component={HatsPage} />
+      </Routes>
     </div>
   );
 }
