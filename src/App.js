7ed941e53add77a78ce266814c66a82c64e63a54
Nobsmoke123 <donaldakobundu@gmail.com>
2025-02-18 03:25:40 +0100

    Add ShopPage and PreviewCollection components; update routing to include ShopPage

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import "./App.css";
 import HomePage from "./pages/home/homepage.component";
 import { Route, Switch } from "react-router-dom";
 
-const HatsPage = () => (
-  <>
-    <h1>Welcome Hats Page</h1>
-  </>
-);
+import { ShopPage } from "./pages/shop/shop.component";
+
+// const HatsPage = () => (
+//   <>
+//     <h1>Welcome Hats Page</h1>
+//   </>
+// );
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
