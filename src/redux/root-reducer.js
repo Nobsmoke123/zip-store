a45a6af9aae5fd058d2a3cb7f0af30dbf579a576
Nobsmoke123 <donaldakobundu@gmail.com>
2025-02-22 00:59:08 +0100

    Implement cart visibility toggle with Redux; add cart reducer, actions, and update Header and CartIcon components

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,9 @@
+import cartReducer from "./cart/cart.reducer";
 import userReducer from "./user/user-reducer";
 
 import { combineReducers } from "redux";
 
 export default combineReducers({
   user: userReducer,
+  cart: cartReducer,
 });
