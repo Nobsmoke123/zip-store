e9f2a58eaf45cd457c0653f95083e4dc0c853328
Nobsmoke123 <donaldakobundu@gmail.com>
2025-02-22 01:26:51 +0100

    Add addItem action and update cart reducer; connect CollectionItem to Redux for adding items to cart

--- src/redux/cart/cart.action.js
+++ src/redux/cart/cart.action.js
@@ -3,3 +3,13 @@ import { CartActionTypes } from "./cart.types";
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN,
 });
+
+/**
+ * Add item redux action
+ * @param {} item
+ * @returns
+ */
+export const addItem = (item) => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item, // the item you want to add
+});
